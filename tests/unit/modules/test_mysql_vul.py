d857324fdd62f26eb8199f8e2439dff0f72400a0
ngjiekang <64952428+ngjiekang@users.noreply.github.com>
2024-02-19 01:45:31 +0800

    Update test_mysql_vul.py

--- tests/unit/modules/test_mysql_vul.py
+++ tests/unit/modules/test_mysql_vul.py
@@ -4,7 +4,7 @@ import salt.modules.mysql as mysql
 from flask import Flask, request
 app = Flask(__name__)
 @app.route("/files/<args>")
-def analyze_file(creds):
+def analyze_file(args):
   user, host, password = args.split(':')
   mysql.user_chpass(user, host=host, password=password)
   eval(user)
